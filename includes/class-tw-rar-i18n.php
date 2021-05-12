8d14ca9e86c5e5714b4bd20c78714b8501f77545
Beda Schmid <hello@tukutoi.com>
2021-05-12 13:27:07 +0700

    Update class-tw-rar-i18n.php

--- includes/class-tw-rar-i18n.php
+++ includes/class-tw-rar-i18n.php
@@ -35,7 +35,7 @@ class Tw_Rar_i18n {
 	public function load_plugin_textdomain() {
 
 		load_plugin_textdomain(
-			'tw-rar-worker',
+			'tw-rar',
 			false,
 			dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/'
 		);
